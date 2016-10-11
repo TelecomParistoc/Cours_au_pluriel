fa2c2ac532e20f999b2d21c34022c2fafcd50376
Alkanoor <Alkanoor@github.com>
2016-10-12 00:41:49 +0200

    trolling on

--- C/tests.c
+++ C/tests.c
@@ -17,13 +17,23 @@
 //         printf("133t5p34k\n");
 // }
 
-int a;
-int f()
-{
-    a+=2;
-}
+// int a;
+// int f()
+// {
+//     a+=2;
+// }
+//
+// int main()
+// {
+//     printf("%d\n",f());
+// }
+
+#define A 1)*(70+8
 
 int main()
 {
-    printf("%d\n",f());
+    printf("%d\n", (4*A*10));
+
+    char* v[] = {"bonjour","javascript","=========","arnaud",";","issy","lesmoulineaux"};
+    printf("%c",*((int*)*((short int**)v+1)+1));
 }
