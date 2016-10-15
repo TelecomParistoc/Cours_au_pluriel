44b958e9136b00a152db296db561e276d2a6d893
Alka <Alkanoor@github.com>
2016-10-15 21:16:07 +0200

    ajouts

--- C/tests.c
+++ C/tests.c
@@ -9,31 +9,26 @@
 //     int a = '0123';
 //     printf("%d\n", a);
 //
-//     a = 258;
+//     int d = 258;
 //     char b = 2;
-//     if((a^b)&255&&!(c-a))
+//     if((d^b)&255&&!(c-d))
 //         printf("leetspeak\n");
 //     else
 //         printf("133t5p34k\n");
-// }
-
-// int a;
-// int f()
-// {
-//     a+=2;
-// }
 //
-// int main()
-// {
-//     printf("%d\n",f());
+//     char* v[] = {"bonjour","javascript","=========","arnaud",";","issy","lesmoulineaux"};
+//     printf("%c\n",*((int*)*((short int**)v+1)+1));
 // }
 
-#define A 1)*(70+8
+
+int a;
+
+int f()
+{
+    a+=2;
+}
 
 int main()
 {
-    printf("%d\n", (4*A*10));
-
-    char* v[] = {"bonjour","javascript","=========","arnaud",";","issy","lesmoulineaux"};
-    printf("%c",*((int*)*((short int**)v+1)+1));
+    printf("%d\n",f());
 }
